e1c54ec660d36f16029b70b0d6da138f45dcbf74
Carlos Jose Zambrano Santos <zambranosantoscarlos@gmail.com>
2026-05-22 20:35:45 -0500

    mi primer worflow

--- src/test/java/bdd/TestRunner.java
+++ src/test/java/bdd/TestRunner.java
@@ -3,8 +3,17 @@ package bdd;
 import com.intuit.karate.junit5.Karate;
 
 public class TestRunner {
+
     @Karate.Test
-    Karate testLogin() {
-        return Karate.run("loginAuth").relativeTo(getClass());
+    Karate testAllByTags() {
+        String tags = System.getProperty("karate.tags");
+        // Escanea TODOS los .feature bajo src/test/java/bdd (y sus sub-paquetes)
+        Karate runner = Karate.run("classpath:bdd");
+
+        if (tags != null && !tags.trim().isEmpty()) {
+            runner = runner.tags(tags.trim());
+        }
+
+        return runner;
     }
 }
